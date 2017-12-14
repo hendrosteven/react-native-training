ef30d260f6607cb10b01245b0d9a7d1bab9fc9a6
Hendro Steven <hendro.steven@gmail.com>
2017-12-14 16:08:48 +0800

    save user

--- src/components/pages/AddUser.js
+++ src/components/pages/AddUser.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {StyleSheet} from 'react-native';
 import {
     Container,
     Content,
@@ -8,42 +9,102 @@ import {
     Body,
     Button,
     Title,
-    CardItem
+    CardItem,Item,Label,Input,
+    Left, Icon, Footer,FooterTab
 } from 'native-base';
 import {Actions} from 'react-native-router-flux';
+
+
 export default class AddUser extends Component {
+
+    constructor(){
+        super();
+        this.state = {
+            fullName: '',
+            phone: '',
+            email: '',
+            address: ''
+        }
+    }
+
+    onSaveUser = () => {
+        console.log(this.state);
+        fetch('https://contact-svr.herokuapp.com/contact', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+                body: JSON.stringify({
+                    fullName: this.state.fullName,
+                    phone: this.state.phone,
+                    email: this.state.email,
+                    address: this.state.address,
+                    photo: ''
+                })
+            })
+            .then(response => response.json())
+            .then(responseJson => {
+               console.log(responseJson);
+            })
+            .catch(error => {
+                console.error(error);
+            });;
+    }
+
     render() {
         return (
             <Container>
                 <Header>
+                    <Left>
+                        <Icon
+                            onPress=
+                            {() => {Actions.pop(); }}
+                            name="arrow-back"
+                            style={{
+                            color: 'white'
+                        }}/>
+                    </Left>
                     <Body>
-                        <Title>Add User</Title>
+                        <Text style={styles.titleText}>Add Users</Text>
                     </Body>
                 </Header>
                 <Content padder>
-                    <Card>
-                        <CardItem>
-                            <Body>
-                                <Text>
-                                    This is Page One, Press button to goto page two
-                                </Text>
-                            </Body>
-                        </CardItem>
-                    </Card>
-                    <Button
-                        dark
-                        bordered
-                        style
-                        ={{
-                        alignSelf: 'center',
-                        margin: 30
-                    }}
-                        onPress=
-                        {() => {Actions.pop(); }}>
-                        <Text>Home</Text>
-                    </Button>
+                    <Item floatingLabel>
+                        <Label>Full Name</Label>
+                        <Input onChangeText={(text) => this.setState({fullName:text})}/>
+                    </Item>
+                    <Item floatingLabel>
+                        <Label>Phone</Label>
+                        <Input onChangeText={(text) => this.setState({phone: text})}/>
+                    </Item>
+                    <Item floatingLabel>
+                        <Label>Email</Label>
+                        <Input onChangeText={(text) => this.setState({email:text})}/>
+                    </Item>
+                    <Item floatingLabel>
+                        <Label>Address</Label>
+                        <Input onChangeText={(text) => this.setState({address:text})}/>
+                    </Item>
                 </Content>
+                <Footer>
+                    <FooterTab>
+                        <Button full onPress={this.onSaveUser}>
+                            <Text>Save User</Text>
+                        </Button>
+                    </FooterTab>
+                </Footer>
             </Container>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    
+        titleText: {
+            fontSize: 20,
+            fontWeight: 'bold',
+            color: 'white'
+        }
+    
+    });
