b550ab6831516d61f6714f14c97e674a17dcc697
Hendro Steven <hendro.steven@gmail.com>
2017-12-15 16:46:33 +0800

    notification onesignal

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import SearchUser from './src/components/pages/SearchUser';
 import DetailUser from './src/components/pages/DetailUser';
 import DinamicText from './src/components/pages/DinamicText';
 import TakePicture from './src/components/pages/TakePicture';
+import OneSignal from 'react-native-onesignal';
 
 export default class App extends Component {
 
@@ -19,11 +20,43 @@ export default class App extends Component {
           <Scene key="addUser" component={AddUser} title="Add User"/>
           <Scene key="searchUser" component={SearchUser} title="Search User"/>
           <Scene key="detailUser" component={DetailUser} title="Detail User"/>
-          <Scene key="dinamicText" component={DinamicText} title="Dinamic Text Input" />
-          <Scene key="takePicture" component={TakePicture} title="Take Picture" />
+          <Scene key="dinamicText" component={DinamicText} title="Dinamic Text Input"/>
+          <Scene key="takePicture" component={TakePicture} title="Take Picture"/>
         </Scene>
       </Router>
     );
   }
-}
 
+  componentWillMount() {
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+    OneSignal.addEventListener('registered', this.onRegistered);
+    OneSignal.addEventListener('ids', this.onIds);
+  }
+
+  componentWillUnmount() {
+    OneSignal.removeEventListener('received', this.onReceived);
+    OneSignal.removeEventListener('opened', this.onOpened);
+    OneSignal.removeEventListener('registered', this.onRegistered);
+    OneSignal.removeEventListener('ids', this.onIds);
+  }
+
+  onReceived(notification) {
+    console.log("Notification received: ", notification);
+  }
+
+  onOpened(openResult) {
+    console.log('Message: ', openResult.notification.payload.body);
+    console.log('Data: ', openResult.notification.payload.additionalData);
+    console.log('isActive: ', openResult.notification.isAppInFocus);
+    console.log('openResult: ', openResult);
+  }
+
+  onRegistered(notifData) {
+    console.log("Device had been registered for push notifications!", notifData);
+  }
+
+  onIds(device) {
+    console.log('Device info: ', device);
+  }
+}
